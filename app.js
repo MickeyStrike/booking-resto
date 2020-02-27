7634c02bbc2b59273c04059ff97d2dbdfb72d6d1
MickeyStrike <53706066+MickeyStrike@users.noreply.github.com>
2020-02-27 23:18:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const router = require('./routes');
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
